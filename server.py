23c90051ef76b98a42f2196ce14e4b74855c4cac
hashfunk <quietbeats@gmail.com>
2019-03-12 11:02:28 -0500

    Initial Commit

---
+++ server.py
@@ -0,0 +1,57 @@
+from flask import Flask, request
+from celery import Celery
+import pyo
+
+app = Flask(__name__)
+app.config.update(CELERY_BROKER_URL='<celery_worker_transport>')
+
+
+def make_celery(app):
+    celery = Celery(
+        app.import_name,
+        broker=app.config['CELERY_BROKER_URL']
+    )
+    celery.conf.update(app.config)
+
+    class ContextTask(celery.Task):
+        def __call__(self, *args, **kwargs):
+            with app.app_context():
+                return self.run(*args, **kwargs)
+
+    celery.Task = ContextTask
+    return celery
+
+
+celery = make_celery(app)
+
+
+class PyoTask(celery.Task):
+    name = "process_temp"
+
+    def __init__(self):
+        print('init was called')
+        self.server = pyo.Server().boot()
+        self.sf = pyo.SfPlayer("<filename>.wav", loop=True)  # replace <filename>
+        self.hr = pyo.Harmonizer(self.sf).out()
+        self.ch = pyo.Chorus(self.sf).out()
+        self.dist = pyo.Disto(self.sf).out()
+        self.dly = pyo.Delay(self.sf).out()
+        self.server.start()
+
+    def run(self, temp):
+        print(f'got {temp} on a worker')
+        mod = int(temp) % 100
+
+        self.hr.mul = mod * 0.12
+        self.hr.transpo = (mod - 50) * 0.25
+
+
+celery.tasks.register(PyoTask)
+
+
+@app.route('/temp', methods=['POST'])
+def temp():
+    data = request.get_data(as_text=True)
+    print(f"Got data : {data}")
+    celery.tasks['process_temp'].delay(data)
+    return 'OK'
