3c88ca45bddc06d1ba29a9a58374fb4b52b2c80f
Clara Brill <clara.brill@gmail.com>
2019-03-19 20:17:52 -0500

    removing celery stuff

--- server.py
+++ server.py
@@ -1,8 +1,7 @@
-from flask import Flask, request
-#from celery import Celery
 import json
+
+from flask import Flask, request
 import redis
-#import pyo
 
 with open('config.json', 'r') as fp:
     config = json.load(fp)
@@ -10,57 +9,10 @@ with open('config.json', 'r') as fp:
 app = Flask(__name__)
 
 r = redis.Redis(host='localhost', port=6379, db=0)
-#app.config.update(CELERY_BROKER_URL=config['celery_broker'])
-
-"""
-def make_celery(app):
-    celery = Celery(
-        app.import_name,
-        broker=app.config['CELERY_BROKER_URL']
-    )
-    celery.conf.update(app.config)
-
-    class ContextTask(celery.Task):
-        def __call__(self, *args, **kwargs):
-            with app.app_context():
-                return self.run(*args, **kwargs)
-
-    celery.Task = ContextTask
-    return celery
-
-
-celery = make_celery(app)
-
-class PyoTask(celery.Task):
-    name = "process_temp"
-
-    def __init__(self):
-        print('init was called')
-        self.server = pyo.Server().boot()
-        self.sf = pyo.SfPlayer(config['wav_file'], loop=True)  # replace <filename>
-        self.hr = pyo.Harmonizer(self.sf).out()
-        self.ch = pyo.Chorus(self.sf).out()
-        self.dist = pyo.Disto(self.sf).out()
-        self.dly = pyo.Delay(self.sf).out()
-        self.server.start()
-
-    def run(self, temp):
-        print(f'got {temp} on a worker')
-        mod = int(temp) % 100
-
-        self.hr.mul = mod * 0.2
-        self.hr.transpo = (mod - 50) * 0.25
-        self.dist.drive = mod * 0.5
-
-
-celery.tasks.register(PyoTask)
-"""
-
 
 @app.route('/temp', methods=['POST'])
 def temp():
     data = request.get_data(as_text=True)
     print(f"Got data : {data}")
-    #celery.tasks['process_temp'].delay(data)
     r.lpush('temp', data)
     return 'OK'
